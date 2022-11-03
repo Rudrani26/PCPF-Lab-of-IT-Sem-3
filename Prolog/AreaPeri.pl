5b701cedbb95910891b5e50e6316ecbee60db44a
Rudrani26 <66739928+Rudrani26@users.noreply.github.com>
2022-11-04 01:43:18 +0530

    Area and Perimeter

---
+++ Prolog/AreaPeri.pl
@@ -0,0 +1,10 @@
+rectangle:-
+  write('Enter Length'),
+  read(L),
+  write('Enter Breadth'),
+  read(B),
+  Peri is 2*(L+B),
+  write("Perimeter is "),write(Peri),nl.
+  Area is L*B,
+  write('Area is '), write(Area),nl
+
