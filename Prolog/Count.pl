3f5fc7b3fe9d112e70d99cf33f107fc9cd1694bd
Rudrani26 <66739928+Rudrani26@users.noreply.github.com>
2022-11-04 01:22:30 +0530

    Update Count.pl

--- Prolog/Count.pl
+++ Prolog/Count.pl
@@ -1,12 +1,10 @@
-count_neg(Ls, P, N):-
-    count_neg(Ls, 0, 0, P, N).
 
-count_neg([],C1,C2,C1,C2).
-count_neg([L|Ls], Cnt1, Cnt2, P, N):-
-    (L > 0 ->
-        C1 is Cnt1 + 1,
-        C2 is Cnt2
+count_neg(Ls, Positive, Negative):-
+    count_neg(Ls, 0, 0, Positive, Negative).
+count_neg( [], C1, C2, C1, C2).
+count_neg([L|Ls], PosCnt, NegCnt, Positive, Negative):-
+    (   L > 0 -> C1 is PosCnt + 1,C2 is NegCnt
     ;
-        C2 is Cnt2 + 1,
-        C1 is Cnt1),
-    count_neg(Ls, C1, C2, P, N).
+    C2 is NegCnt + 1, C1 is PosCnt),
+    count_neg( Ls, C1, C2, Positive, Negative).
+
